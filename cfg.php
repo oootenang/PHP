85a13399abffb623d2232ad6ac3795793bad9e08
oootenang <114143596+oootenang@users.noreply.github.com>
2022-10-02 07:39:58 +0800

    Add files via upload

---
+++ cfg.php
@@ -0,0 +1,6 @@
+<?
+
+$Cookie = "HstCfa4531111=1654690411732; __dtsu=51A016546904530E1180ECBADEB718DF; _ga=GA1.2.743942254.1654691005; HstCmu4531111=1660287567832; dom3ic8zudi28v8lr6fgphwffqoz0j6c=ee2cc0c3-ab60-4616-b37d-05b444330b02:3:1; PHPSESSID=04eeb98346a537911de4afe6f244ae6c; HstCnv4531111=4; HstCns4531111=5; _data_cpc=77-5; _data_cpm=11-1; _data_html=11-1_93-1_108-1_109-1_136-1; prefetchAd_5295692=true; pop_delay_1336=1; HstCla4531111=1660373751081; HstPn4531111=39; HstPt4531111=198";
+
+
+$UserAgent = "Mozilla/5.0 (Linux; Android 11; SM-A032F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.136 Mobile Safari/537.36";
